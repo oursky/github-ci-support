0882ae306b6ed6780bd67248620f8c4588141557
kiootic <kiootic@oursky.com>
2022-07-13 13:14:30 +0800

    Fix SDK manager location

--- setup-android/src/index.ts
+++ setup-android/src/index.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
 import * as exec from "@actions/exec";
 import * as io from "@actions/io";
+import * as glob from "@actions/glob";
 import path from "path";
 import fs from "fs";
 
@@ -18,16 +19,33 @@ export async function setupAndroid(accept: string, packages: string) {
     return;
   }
   core.info(`Android HOME: ${androidHome}`);
+  const sdkManager = await locateSDKManager(androidHome);
+  core.info(`SDK Manager: ${sdkManager}`);
 
-  await acceptLicenses(accept);
+  await acceptLicenses(sdkManager, accept);
 
   const packageList = packages.split(" ").filter((x) => x.length > 0);
   await restorePackages(androidHome, packageList);
-  await installPackages(packageList);
+  await installPackages(sdkManager, packageList);
   await cachePackages(androidHome, packageList);
 }
 
-async function acceptLicenses(accept: string) {
+async function locateSDKManager(androidHome: string) {
+  const globber = await glob.create(
+    `${androidHome}/cmdline-tools/*/bin/sdkmanager`
+  );
+  let sdkManager = "";
+  for await (const p of globber.globGenerator()) {
+    sdkManager = p;
+    break;
+  }
+  if (sdkManager.length === 0) {
+    throw new Error("SDK manager not found in ANDROID_HOME");
+  }
+  return sdkManager;
+}
+
+async function acceptLicenses(sdkManager: string, accept: string) {
   if (accept.toLowerCase() !== "y") {
     core.error('Must pass "y"/"Y" to accept-licenses input');
     return;
@@ -35,7 +53,7 @@ async function acceptLicenses(accept: string) {
 
   await core.group("Accepting licenses...", async () => {
     const stdin = Buffer.from((accept + "\n").repeat(100), "utf8");
-    await exec.exec("sdkmanager", ["--licenses"], { input: stdin });
+    await exec.exec(sdkManager, ["--licenses"], { input: stdin });
   });
 }
 
@@ -53,11 +71,11 @@ async function restorePackages(androidHome: string, packages: string[]) {
   }
 }
 
-async function installPackages(packages: string[]) {
+async function installPackages(sdkManager: string, packages: string[]) {
   await core.group("Installing packages...", async () => {
     // Installing packages with same dependency would double-install the dependency.
     // Ignore that since it's mostly one-off extra downloads.
-    await exec.exec("sdkmanager", packages);
+    await exec.exec(sdkManager, packages);
   });
 }
 
